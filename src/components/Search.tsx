59b26a7ec494af8931691a9a0465b30a2bd47ea5
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-21 20:54:18 +1000

    added useRef example that references button element and accesses click method

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { Results } from "./Results";
 import { Item } from "./types";
@@ -8,6 +8,11 @@ export const Search = () => {
   const [result, setResult] = useState<Item[]>([]);
 
   console.log("SEARCH rendering");
+
+  // useRef used to reference the button element which allows access to the click method
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {});
 
   const fetchBook = async (bookTitle: string) => {
     const response = await axios.get(
@@ -29,9 +34,20 @@ export const Search = () => {
 
   return (
     <div>
+      <div
+        onClick={() => buttonRef.current?.click()}
+        style={{
+          backgroundColor: "salmon",
+          cursor: "pointer",
+        }}
+      >
+        Custom Search Button using ref (button)
+      </div>
       <form onSubmit={handleSubmit}>
         <input name="query" value={query} onChange={handleChange} />
-        <button type="submit">Submit</button>
+        <button type="submit" ref={buttonRef}>
+          Submit
+        </button>
       </form>
       <hr />
       {result.length > 0 && <Results items={result} />}
